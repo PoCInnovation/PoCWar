196e8aedd087e2c2028149573e8a3b8d4aaf7265
jerome <jerome.collet@epitech.eu>
2020-06-01 13:54:26 +0200

    ADD python language post

--- backend/index_docker_exec.js
+++ backend/index_docker_exec.js
@@ -5,9 +5,6 @@ app.use(bodyParser.json());
 var jsonParser = bodyParser.json()
 const { exec } = require("child_process");
 var cors = require("cors");
-const {Docker} = require('node-docker-api');
-
-const docker_clang = new Docker({ socketPath: '/var/run/docker.sock' });
 
 app.listen(4000, () => {
     console.log("listening on port 4000");
@@ -16,7 +13,7 @@ app.listen(4000, () => {
 app.use(cors());
 
 app.post('/clang', jsonParser, function (req, res) {
-    console.log("request POST on /.");
+    console.log("request POST on /clang.");
     fs = require('fs');
     fs.writeFile('test.c', req.body.code, function (err) {
         if (err) return console.log(err);
@@ -26,6 +23,25 @@ app.post('/clang', jsonParser, function (req, res) {
         if (error) {
             console.log(`error: ${error.message}`);
             res.status(500).send(`error in docker build : ${error.message}`);
+            return;
+        }
+        console.log(`stdout: ${stdout} stderr: ${stderr}`);
+        res.status(200).send(`{"stdout":"${stdout}", "stderr":"${stderr}"}`);
+    });
+});
+
+app.post('/python', jsonParser, function (req, res) {
+    console.log("request POST on /python.");
+    fs = require('fs');
+    fs.writeFile('test.py', req.body.code, function (err) {
+        if (err) return console.log(err);
+        console.log('file write in test.py');
+    });
+    exec(`docker run -v "${process.cwd()}:/execution" python_app`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.status(500).send(`error in docker build : ${error.message}`);
+            return;
         }
         console.log(`stdout: ${stdout} stderr: ${stderr}`);
         res.status(200).send(`{"stdout":"${stdout}", "stderr":"${stderr}"}`);

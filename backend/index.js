1eb0bdb39af41ec1914663d7bb9c617720b4e662
jerome <jerome.collet@epitech.eu>
2020-06-01 14:15:08 +0200

    CHANGE index.js to the new code

--- backend/index.js
+++ backend/index.js
@@ -13,53 +13,55 @@ app.listen(4000, () => {
 app.use(cors());
 
 app.post('/clang', jsonParser, function (req, res) {
-    console.log("request GET on /.");
+    console.log("request POST on /clang.");
     fs = require('fs');
     fs.writeFile('test.c', req.body.code, function (err) {
         if (err) return console.log(err);
         console.log('file write in test.c');
     });
-    exec(`gcc -o test ${process.cwd()}/test.c`, (error, stdout, stderr) => {
+    exec(`docker run -v "${process.cwd()}:/execution" app`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
+            res.status(500).send(`error in docker run : ${error.message}`);
             return;
         }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        exec(`${process.cwd()}/test`, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-            }
-            console.log(`stdout: ${stdout}`);
-            res.status(200).send(`${stdout}`);
-            return;
-        });
+        console.log(`stdout: ${stdout} stderr: ${stderr}`);
+        res.status(200).send(`{"stdout":"${stdout}", "stderr":"${stderr}"}`);
     });
 });
 
 app.post('/python', jsonParser, function (req, res) {
-    console.log("request GET on /.");
+    console.log("request POST on /python.");
     fs = require('fs');
     fs.writeFile('test.py', req.body.code, function (err) {
         if (err) return console.log(err);
-        console.log('file write in test.c');
+        console.log('file write in test.py');
     });
-    exec(`python3 ${process.cwd()}/test.py`, (error, stdout, stderr) => {
+    exec(`docker run -v "${process.cwd()}:/execution" python_app`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
+            res.status(500).send(`error in docker run : ${error.message}`);
             return;
         }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
+        console.log(`stdout: ${stdout} stderr: ${stderr}`);
+        res.status(200).send(`{"stdout":"${stdout}", "stderr":"${stderr}"}`);
+    });
+});
+
+app.post('/javascript', jsonParser, function (req, res) {
+    console.log("request POST on /javascript.");
+    fs = require('fs');
+    fs.writeFile('test.js', req.body.code, function (err) {
+        if (err) return console.log(err);
+        console.log('file write in test.js');
+    });
+    exec(`docker run -v "${process.cwd()}:/execution" javascript_app`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.status(500).send(`error in docker run : ${error.message}`);
             return;
         }
-        console.log(`stdout: ${stdout}`);
+        console.log(`stdout: ${stdout} stderr: ${stderr}`);
+        res.status(200).send(`{"stdout":"${stdout}", "stderr":"${stderr}"}`);
     });
 });

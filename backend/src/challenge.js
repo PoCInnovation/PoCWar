c73e4564c1da2cc5c600f401c413e8b23ed5def9
Théo <theo.hinsinger@gmail.com>
2020-07-22 23:19:28 +0200

    fix challenge test generation, build_all for Dockerfiles

--- backend/src/challenge.js
+++ backend/src/challenge.js
@@ -3,33 +3,59 @@ const admin = require('firebase-admin');
 require('firebase/storage');
 
 function generateTests(challenge) {
-    let command = "";
-
-    Object.entries(challenge).forEach(([suiteName, tests], _) => {
-        command += `"${suiteName}": [`;
-        tests.forEach((test, index) => {
-            command += `$(execTest "${test.args.join(" ")}" "${test.stdout}" "${test.stderr}" ${test.code})${tests.length - 1 !== index ? ',' : ''}`;
-        });
-        command += "],";
-    });
-    return `#!/bin/bash
+    let command = `#!/bin/bash
 
 # $1 => args
 # $2 => stdout
 # $3 => stderr
 # $4 => exit status
-execTest() {
+exec_test() {
     eval "$({ berr=$({ bout=$(eval "./bin.out" $1); bret=$?; } 2>&1; declare -p bout bret >&2); declare -p berr; } 2>&1)"
-    status=""
-    if [ "$bout" = "$2" ] && [ "$berr" = "$3" ] && [ $bret -eq $4 ]; then
-        status="passed"
+
+    if [ "$bout" != "$2" ]; then
+        echo -ne "{\\"status\\":\\"failed\\",\\"error\\":\\"stdout\\",\\"stdout\\":{\\"expected\\":\\"$2\\",\\"actual\\":\\"$bout\\"},"
+    elif [ "$berr" != "$3" ]; then
+        echo -ne "{\\"status\\":\\"failed\\",\\"error\\":\\"stderr\\",\\"stderr\\":{\\"expected\\":\\"$3\\",\\"actual\\":\\"$berr\\"},"
+    elif [ $bret -ne $4 ]; then
+        echo -ne "{\\"status\\":\\"failed\\",\\"error\\":\\"ret\\",\\"ret\\":{\\"expected\\":\\"$3\\",\\"actual\\":\\"$bret\\"},"
     else
-        status="failed"
+        return 0
     fi
-    echo -ne "{\\"status\\":\\"$status\\", \\"code\\":\\"$bret\\", \\"stdout\\":\\"$bout\\", \\"stderr\\":\\"$berr\\"}"
+    return 1
 }
 
-echo {${command.substring(0, command.length - 1)}}`;
+echo -ne '{"suites":['
+`;
+    const suiteCount = Object.keys(challenge).length;
+    Object.entries(challenge).forEach(([suiteName, tests], suiteIndex) => {
+        let array = "";
+        tests.forEach((test, index) => {
+            array += `        "'${test.args.join(' ')}' '${test.stdout}' '${test.stderr}' ${test.code}"\n`;
+        });
+        command += `exec_suite_${suiteName}() {
+    declare -a array=(
+${array}
+    )
+
+    test_count=${tests.length}
+
+    for i in "\${!array[@]}"
+    do
+        eval exec_test \${array[$i]}
+        if [ $? -ne 0 ]; then
+            break
+        fi
+        if [ $i -eq $(($test_count - 1)) ]; then
+            echo -ne '{"status":"passed",'
+        fi
+    done
+    echo -ne "\\"name\\":\\"${suiteName}\\",\\"test_passed\\":$i,\\"test_count\\":$test_count}${suiteIndex !== suiteCount - 1 ? ',' : ''}"
+}
+
+exec_suite_${suiteName}`;
+    });
+    console.log(command + '\necho "]}"');
+    return command + '\necho "]}"';
 }
 
 function addChallenge(req, res) {
